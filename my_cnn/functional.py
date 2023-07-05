8c586a341e1ff5fe9c0301a04ff704b0d20ff899
Shmuley <shmuley228@gmail.com>
2023-07-05 19:09:03 +0600

    Move convolution operation to convolutional layer

--- my_cnn/functional.py
+++ my_cnn/functional.py
@@ -9,19 +9,6 @@ def ReLU_grad(output_grad, output):
     '''Relu activation function gradient'''
     return output_grad * (output > 0).astype(float)
 
-def convolve_2d(image, kernel):
-    height, width = image.shape[:2]
-    n_kernels, _, kernel_size = kernel.shape
-
-    out = np.zeros((
-        height - kernel_size + 1,
-        width - kernel_size + 1,
-        n_kernels
-    ))
-    for slice, i, j in conv_slices(image, kernel_size):
-        out[i, j] = np.sum(slice * kernel,
-                           axis = (1, 2))
-
 def conv_slices(image, kernel_size):
     '''A generator that yields image slices for convolution'''
     height, width = image.shape[:2]
