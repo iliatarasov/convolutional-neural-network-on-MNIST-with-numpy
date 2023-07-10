493d3780046d761997907260d7a8fdf1625e6268
Shmuley <shmuley228@gmail.com>
2023-07-10 17:05:22 +0600

    Add metrics tracking and model pickling

--- my_cnn/CNN.py
+++ my_cnn/CNN.py
@@ -1,6 +1,9 @@
 import functools
+import pickle
+import warnings
 import numpy as np
 from collections import defaultdict
+from sklearn.metrics import balanced_accuracy_score, accuracy_score, recall_score, precision_score
 
 from .layers import Convolutional, MaxPooling, Linear
 
@@ -12,17 +15,22 @@ class Classifier:
         'pool': MaxPooling,
         'lin': Linear,
     }
-    def __init__(self, architecture, params, n_classes):
+    def __init__(self, architecture, params, n_classes, input_size):
         '''
         Arguments:
-            architecture (str or list[str]): sequence of layer names:
+            architecture (str or list[str]): sequence of layer names.
+            Accepted layer names: conv, pool, lin. Expected input:
                 if str: layer names with spaces in between
                 if list: layer names (str) in list
-            params (list): list of parameters per layer. Expected inputs
-            for all layer types:
-                conv: (number of kernels, kernel size)
-                pool: (kernel size)
-                lin:  (input size, output size)
+            
+            params (list[tuple[int]]): list of parameters per layer. Expected inputs
+            per layer type:
+                conv (tuple): (number of kernels, kernel size)
+                pool (tuple): (kernel size)
+                lin  (tuple): (output size)
+
+            n_classes (int): number of classes in data
+            input_size (tuple[int] or like): size of input
         '''
         self.trained = False
         self.n_classes = n_classes
@@ -31,6 +39,12 @@ class Classifier:
         assert len(architecture) == len(params), 'Architecture and parameters length do not match'
         self.layers = []
         for i, layer_type in enumerate(architecture):
+            if layer_type == 'lin':
+                #This gets input size for a linear layer by doing a 
+                # forward pass with an empty matrix
+                assert self.layers, 'Linear layer can not be first'
+                dummy = functools.reduce(lambda x, y: y(x), self.layers, np.zeros(input_size))
+                params[i] = (np.product(dummy.shape), params[i])                    
             self.layers.append(self.LAYERS[layer_type](*params[i]))
 
     def forward(self, image):
@@ -43,13 +57,9 @@ class Classifier:
         layers = [layer.backward for layer in self.layers[::-1]]
         return functools.reduce(lambda x, y: y(x), layers, output_grad)
 
-    def fit(self, X_train, y_train, learning_rate=1e-3,
-            n_epochs=20, show_progress=True):
+    def fit(self, X_train, y_train, learning_rate=1e-3, n_epochs=20):
         '''Training routine'''
         train_size = len(X_train)
-
-        if show_progress == True:
-            show_progress = 1
 
         self.metrics = defaultdict(list)
 
@@ -60,7 +70,7 @@ class Classifier:
 
             for sample in range(train_size):
                 mean_loss = np.mean(running_loss) if running_loss else 0
-                print(f'Sample {sample}/{train_size}, loss {mean_loss}', end='\r')
+                print(f'Sample {sample}/{train_size}, loss {mean_loss:.5f}', end='\r')
                 image = X_train[sample]
 
                 label = np.array(y_train[sample])
@@ -76,9 +86,15 @@ class Classifier:
 
                 running_loss.append(loss)
             print()
-            self.metrics['loss'].append(np.mean(running_loss))
-            
 
+            self.metrics['accuracy'].append(accuracy_score(y_train, y_pred))
+            self.metrics['balanced accuracy'].append(balanced_accuracy_score(y_train, y_pred))
+            self.metrics['recall'].append(recall_score(y_train, y_pred, average='micro'))
+            self.metrics['precision'].append(precision_score(y_train, y_pred, average='micro'))
+            self.metrics['loss'].append(np.mean(running_loss))
+            print(f'Epoch {epoch}\tloss: {np.mean(running_loss):.3f}\t',\
+                    f'balanced accuracy on train: {balanced_accuracy_score(y_train, y_pred):.3f}')
+            
         self.trained = True
 
 
@@ -90,7 +106,7 @@ class Classifier:
     def predict(self, image):
         assert self.trained, 'The network was never trained'
         prediction = self.forward(image)
-        return prediction.argmax()
+        return prediction.argmax()                
 
     def show_parameters(self):
         '''Prints a description of the network'''
@@ -99,4 +115,11 @@ class Classifier:
             for param_name, param_value in layer.params.items():
                 print(f'\t{param_name}: {param_value}')
 
-
+    def save(self, path):
+        '''Saves the model as pickle'''
+        if not self.trained:
+            warnings.warn('Saving an untrained model')
+        if not path.endswith('.pkl'):
+            path += '.pkl'
+        with open(path, 'wb') as file:
+            pickle.dump(self, file)
